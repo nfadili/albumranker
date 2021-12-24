6715699b3bdefbbb221cb28fba5d67d4ad8ce1dc
Nabil Fadili <nhfadili@gmail.com>
2021-12-24 12:40:50 -0800

    add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    jsxBracketSameLine: false,
+    jsxSingleQuote: true,
+    printWidth: 100,
+    quoteProps: 'as-needed',
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    useTabs: false,
+    trailingComma: 'none'
+};

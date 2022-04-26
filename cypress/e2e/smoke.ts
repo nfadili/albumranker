116215f80f1afdbd1a039d0220d86fdea0d79978
nfadili <nhfadili@gmail.com>
2022-04-26 16:34:52 -0700

    cleanup cypress

--- cypress/e2e/smoke.ts
+++ cypress/e2e/smoke.ts
@@ -6,21 +6,6 @@ describe('smoke tests', () => {
     });
 
     it('should allow you to register and login', () => {
-        const loginForm = {
-            email: `${faker.internet.userName()}@example.com`,
-            password: faker.internet.password()
-        };
-        cy.then(() => ({ email: loginForm.email })).as('user');
-
-        cy.visit('/');
-        cy.findByRole('link', { name: /sign up/i }).click();
-
-        cy.findByRole('textbox', { name: /email/i }).type(loginForm.email);
-        cy.findByLabelText(/password/i).type(loginForm.password);
-        cy.findByRole('button', { name: /create account/i }).click();
-
-        cy.findByRole('link', { name: /notes/i }).click();
-        cy.findByRole('button', { name: /logout/i }).click();
-        cy.findByRole('link', { name: /log in/i });
+        // TODO
     });
 });

379d023bf88dcaaed2d0b176703ebcc4d9207de1
nfadili <nhfadili@gmail.com>
2022-12-04 12:54:08 -0800

    fix type errors after mantine upgrade

--- app/components/LinkButton.tsx
+++ app/components/LinkButton.tsx
@@ -1,11 +1,13 @@
 import type { ButtonProps as _ButtonProps } from '@mantine/core';
 import { Button } from '@mantine/core';
-import type { LinkProps } from '@remix-run/react';
 import { Link } from '@remix-run/react';
 
-type ButtonProps = Omit<_ButtonProps<LinkProps>, 'component'>;
-
-export const LinkButton = ({ to, children, ...props }: ButtonProps & LinkProps) => {
+import type { LinkProps } from '@remix-run/react';
+export const LinkButton = ({
+    to,
+    children,
+    ...props
+}: _ButtonProps & React.ComponentPropsWithoutRef<'button'> & { label: string } & LinkProps) => {
     return (
         <Button<typeof Link> component={Link} to={to} {...props}>
             {children}
--- app/components/LinkText.tsx
+++ app/components/LinkText.tsx
@@ -1,11 +1,10 @@
 import type { TextProps as _TextProps } from '@mantine/core';
 import { Text } from '@mantine/core';
-import type { LinkProps } from '@remix-run/react';
 import { Link } from '@remix-run/react';
 
-type TextProps = Omit<_TextProps<LinkProps>, 'component'>;
+import type { LinkProps } from '@remix-run/react';
 
-export const LinkText = ({ to, children, ...props }: TextProps & LinkProps) => {
+export const LinkText = ({ to, children, ...props }: _TextProps & LinkProps) => {
     return (
         <Text<typeof Link> component={Link} to={to} {...props}>
             {children}
